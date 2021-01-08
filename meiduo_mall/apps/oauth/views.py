2e711e4d6b51b027a59ecc3776c39f1f73321d7e
zzx <zzx_092721@163.com>
2021-01-08 20:25:23 +0800

    对openid进行加密解密处理

--- meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/apps/oauth/views.py
@@ -2,12 +2,14 @@ from QQLoginTool.QQtool import OAuthQQ
 from django.views import View
 from django.http import *
 
+
 class QQUserView(View):
     """用户扫码登录的回调处理"""
+
     def get(self, request):
         code = request.GET.get('code')
         if code is None:
-            return JsonResponse({'code':400, 'errmsg':"没有code参数"})
+            return JsonResponse({'code': 400, 'errmsg': "没有code参数"})
         # QQ登录参数
         # 我们申请的 客户端id
         QQ_CLIENT_ID = '101474184'
@@ -27,12 +29,14 @@ class QQUserView(View):
         try:
             qquser = OAuthQQUser.objects.get(openid=openid)
         except:
-            return JsonResponse({'code':300, 'access_token': openid})
+            from apps.oauth.utils import generate_access_token
+            token = generate_access_token(openid)
+            return JsonResponse({'code': 300, 'access_token': token})
         else:
             from django.contrib.auth import login
             login(request, qquser.user)
             response = JsonResponse({'code': 0, 'errmsg': "OK"})
-            response.set_cookie('username',qquser.user.username, max_age=14*24*3600)
+            response.set_cookie('username', qquser.user.username, max_age=14 * 24 * 3600)
             return response
 
     def post(self, request):
@@ -41,26 +45,29 @@ class QQUserView(View):
         mobile = data.get('mobile')
         password = data.get('password')
         sms_code = data.get('sms_code')
-        openid = data.get('access_token')
-
+        access_token = data.get('access_token')
+        from apps.oauth.utils import check_access_token
+        openid = check_access_token(access_token)
+        if openid is None:
+            return JsonResponse({'code': 400, 'errmsg': "绑定失败"})
         # 根据手机号判断用户信息
         from apps.users.models import User
         try:
             user = User.objects.get(mobile=mobile)
         except:
-            user = User.objects.create_user(username = mobile,
+            user = User.objects.create_user(username=mobile,
                                             mobile=mobile,
                                             password=password)
         else:
             if not user.check_password(password):
-                return JsonResponse({'code':400 ,'errmsg': "绑定失败"})
+                return JsonResponse({'code': 400, 'errmsg': "绑定失败"})
             # 绑定用户信息
             from apps.oauth.models import OAuthQQUser
-            OAuthQQUser.objects.create(openid=openid,user=user)
+            OAuthQQUser.objects.create(openid=openid, user=user)
             # 状态保持
             from django.contrib.auth import login
             login(request, user)
             # 设置cookie
-            response = JsonResponse({'code':0 , 'errmsg':'OK'})
-            response.set_cookie('username',user.username ,max_age=14*24*3600)
+            response = JsonResponse({'code': 0, 'errmsg': 'OK'})
+            response.set_cookie('username', user.username, max_age=14 * 24 * 3600)
             return response
