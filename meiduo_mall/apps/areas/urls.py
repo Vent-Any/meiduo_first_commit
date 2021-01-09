a498ccb4be5eff4ed8563085e3fbb691fea585c4
zzx <zzx_092721@163.com>
2021-01-09 23:38:19 +0800

    实现查询市区县数据

--- meiduo_mall/apps/areas/urls.py
+++ meiduo_mall/apps/areas/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from apps.areas.views import *
 urlpatterns = [
     path('areas/', ProvienceView.as_view()),
+    path('areas/<pk>/', SubAreaView.as_view()),
 ]
--- meiduo_mall/apps/areas/views.py
+++ meiduo_mall/apps/areas/views.py
@@ -19,3 +19,19 @@ class ProvienceView(View):
             })
         # 返回响应
         return JsonResponse({'code': 0, 'errmsg': "OK", 'province_list': province_list})
+
+class SubAreaView(View):
+    def get(self, request, pk):
+        # 接受参数
+        # 1. 接收参数 因为前端传递的 上一级id 在url中,所以我们在url中已经获取到了 上一级id
+        # pk 就是 parent_id
+        # 2. 根据 parent_id 进行查询
+        sub_areas = Area.objects.filter(parent_id=pk)
+        sub_list = []
+        for item in sub_areas:
+            sub_list.append({
+                'id':item.id,
+                'name':item.name
+            })
+        # 返回数据
+        return JsonResponse({'code': 0, 'errmsg':"OK", 'sub_data': {'subs': sub_list}})
