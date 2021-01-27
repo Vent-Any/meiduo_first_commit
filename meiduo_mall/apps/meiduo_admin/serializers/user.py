3aef1440a5b8e572fb6f83724a42302c9c0ade6c
Anty <zzx_092721@163.com>
2021-01-27 21:53:16 +0800

    实现添加用户

--- meiduo_mall/apps/meiduo_admin/serializers/user.py
+++ meiduo_mall/apps/meiduo_admin/serializers/user.py
@@ -5,4 +5,13 @@ class UserModelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id','username','mobile','email']
+        fields = ['id','username','mobile','email','password']
+        extra_kwargs = {
+            'password':{
+                'write_only':True,
+                'max_length':20,
+                'min_length':5
+            }
+        }
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
