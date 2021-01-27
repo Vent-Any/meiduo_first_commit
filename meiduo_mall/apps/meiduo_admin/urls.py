f9c83df0ce590024e061085ab3eb4d75b6ffeee7
Anty <zzx_092721@163.com>
2021-01-27 21:29:38 +0800

    实现查询用户展示

--- meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/apps/meiduo_admin/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from apps.meiduo_admin.login import admin_obtain_token
-from apps.meiduo_admin.views import home
+from apps.meiduo_admin.views import home, user
 
 urlpatterns = [
     path('authorizations/', admin_obtain_token),
     # 日活用户统计
     path('statistical/day_active/', home.UserActiveAPIView.as_view()),
+    # 日下单统计
     path('statistical/day_orders/', home.UserOrderAPIView.as_view()),
+    # 月增用户统计
     path('statistical/month_increment/', home.MonthUserAPIView.as_view()),
+    # 查询用户展示
+    path('users/', user.UserListAPIView.as_view()),
 
 ]
