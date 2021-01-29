2b5c41055a83ef07343ed42d766eedb5adc17108
Anty <zzx_092721@163.com>
2021-01-29 14:45:34 +0800

    实现事务以及SKU更新

--- meiduo_mall/apps/meiduo_admin/serializers/sku.py
+++ meiduo_mall/apps/meiduo_admin/serializers/sku.py
@@ -1,5 +1,14 @@
-from apps.goods.models import SKU, GoodsCategory, SPU, SPUSpecification
+from apps.goods.models import SKU, GoodsCategory, SPU, SPUSpecification, SKUSpecification
 from rest_framework import serializers
+
+
+class SKUSpecificationModelSerializer(serializers.ModelSerializer):
+    spec_id = serializers.IntegerField()
+    option_id = serializers.IntegerField()
+
+    class Meta:
+        model = SKUSpecification
+        fields = ['spec_id', 'option_id']
 
 
 class SKUModelSerializer(serializers.ModelSerializer):
@@ -8,9 +17,38 @@ class SKUModelSerializer(serializers.ModelSerializer):
     # 对应模型返回数值
     category = serializers.StringRelatedField()
 
+    spu = serializers.StringRelatedField()
+    spu_id = serializers.IntegerField()
+
+    specs = SKUSpecificationModelSerializer(many=True)
+
     class Meta:
         model = SKU
         fields = '__all__'
+
+    def create(self, validated_data):
+        specs = validated_data.pop('specs')
+        from django.db import transaction
+        with transaction.atomic():
+            save_point = transaction.savepoint()
+            try:
+                sku = SKU.objects.create(**validated_data)
+                for spec in specs:
+                    SKUSpecification.objects.create(sku=sku, **spec)
+            except:
+                transaction.savepoint_rollback(save_point)
+
+            else:
+                transaction.savepoint_commit(save_point)
+        return sku
+
+    def update(self, instance, validated_data):
+        specs = validated_data.pop('specs')
+        super().update(instance,validated_data)
+        for spec in specs:
+            SKUSpecification.objects.filter(sku=instance,spec_id=spec.get('spec_id')).update(option_id=spec.get('option_id'))
+
+        return instance
 
 
 class GoodsCategoryModelSerializer(serializers.ModelSerializer):
@@ -38,6 +76,7 @@ class SPUSpecsModelSerializer(serializers.ModelSerializer):
     spu = serializers.StringRelatedField()
     spu_id = serializers.IntegerField()
     options = SpecificationOptionModelSerializer(many=True)
+
     class Meta:
         model = SPUSpecification
-        fields = ['id', 'name', 'spu', 'spu_id','options']
+        fields = ['id', 'name', 'spu', 'spu_id', 'options']
