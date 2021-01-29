943900bad12223029acd55cf965dfa74caf588d3
Anty <zzx_092721@163.com>
2021-01-29 13:48:29 +0800

    实现分类的文字显示

--- meiduo_mall/apps/meiduo_admin/serializers/sku.py
+++ meiduo_mall/apps/meiduo_admin/serializers/sku.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 
 
 class SKUModelSerializer(serializers.ModelSerializer):
+    # 对应外键值
+    category_id = serializers.IntegerField()
+    # 对应模型返回数值
+    category = serializers.StringRelatedField()
+
     class Meta:
         model = SKU
         fields = '__all__'
